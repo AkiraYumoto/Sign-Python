967be7598557bce54e5b0f65d103d1f41ff14c57
AkiraYumoto <juanicaehd@gmail.com>
2025-11-01 22:24:29 -0300

    Agregado maximo de manos detectables, y mas letras del lenguaje

--- inference_classifier.py
+++ inference_classifier.py
@@ -13,10 +13,14 @@ mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
-hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+hands = mp_hands.Hands(static_image_mode=False,max_num_hands=1, min_detection_confidence=0.3)
 
 
-labels_dict = {0: 'B', 1: 'C', 2: 'D'}
+labels_dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 
+    6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L', 
+    12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 
+    18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 
+    24: 'Y', 25: 'Z', 26:'No se'}
 while True:
 
     data_aux = []
@@ -68,7 +72,8 @@ while True:
                     cv2.LINE_AA)
 
     cv2.imshow('frame', frame)
-    cv2.waitKey(1)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 
 cap.release()
